4fa45774b2f3481c0ec7edcd7273039fa448fac2
SasidharMunduri <163239960+SasidharMunduri@users.noreply.github.com>
2024-03-18 18:08:03 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Sum {
+    public static void main(String[] args)
+    {
+        int []values={3,5,739,41,55,66};
+        Arrays.sort(values);
+        int length=values.length;
+        int sum=values[length-2]+values[length-3];
+        System.out.println(sum);
+
+    }
